3220a7240b992b335174ab12ad1acacae0aeb048
“Cassandra” <cassandraperumal5@gmail.com>
2022-12-08 11:38:27 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,8 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
-{
+int main(void)
+{ 
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
